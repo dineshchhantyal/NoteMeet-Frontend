fb3ce8d4005bbb2cac7a868481cec0fb0680c86a
Valik <valik3201@gmail.com>
2024-10-12 13:15:14 +0200

    feat: Add email verification for unverified users during login and registration

--- actions/register.ts
+++ actions/register.ts
@@ -2,10 +2,12 @@
 
 import * as z from "zod";
 import bcrypt from "bcryptjs";
-import { db } from "@/lib/db";
 
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -32,7 +34,11 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     },
   });
 
+  const verificationToken = await generateVerificationToken(email);
+
+  await sendVerificationEmail(verificationToken.email, verificationToken.token);
+
   return {
-    success: "User created!",
+    success: "Confirmation email sent!",
   };
 };
