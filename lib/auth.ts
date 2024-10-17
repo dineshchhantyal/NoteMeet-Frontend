8cdc656b0bf90fc169aadafa0a095f1f35460174
Valik <valik3201@gmail.com>
2024-10-17 17:11:52 +0200

    refactor: rename currentuser and add currentRole function

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,13 @@
 import { auth } from "@/auth";
 
-export const currentuser = async () => {
+export const currentUser = async () => {
   const session = await auth();
 
   return session?.user;
+};
+
+export const currentRole = async () => {
+  const session = await auth();
+
+  return session?.user.role;
 };
