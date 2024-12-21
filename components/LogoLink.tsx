d9b41230822f1a7c8da6e729f503c8c6b178e2ca
Dinesh Chhantyal <myagdichhantyal@gmail.com>
2024-12-21 00:11:03 -0600

    update logo image paths from logo.jpeg to logo.png in LogoLink and Footer components

--- components/LogoLink.tsx
+++ components/LogoLink.tsx
@@ -8,7 +8,7 @@ interface LogoLinkProps {
 const LogoLink = ({ showText = true }: LogoLinkProps) => {
     return (
         <Link href="/" className="flex items-center space-x-2">
-            <Image src="/logo.jpeg" alt="NoteMeet Logo" width={40} height={40} />
+            <Image src="/logo.png" alt="NoteMeet Logo" width={40} height={40} />
             {showText && (
                 <span className="text-2xl text-foreground"><span className="font-bold">note</span>meet</span>
             )}
