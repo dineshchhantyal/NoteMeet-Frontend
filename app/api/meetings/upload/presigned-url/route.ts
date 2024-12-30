da0f9d8ce77a6313d0fb0629e3552a9a883ef204
Dinesh Chhantyal <myagdichhantyal@gmail.com>
2024-12-29 22:12:43 -0600

    create meeting if not found and improve error handling in presigned URL generation

--- app/api/meetings/upload/presigned-url/route.ts
+++ app/api/meetings/upload/presigned-url/route.ts
@@ -3,60 +3,60 @@ import { db } from '@/lib/db';
 import { currentUser } from '@/lib/auth';
 import { MeetingStatus } from '@/types/meeting';
 import { generatePresignedUrl, S3BucketType } from '@/lib/presigned-url';
+import { v4 as uuidv4 } from 'uuid';
 
 export async function GET(request: NextRequest) {
-	const { searchParams } = request.nextUrl;
-
-	const user = await currentUser();
-	if (!user) {
-		return Response.json({ message: 'Unauthorized' }, { status: 401 });
-	}
-
-	if (!user.isEarlyAccess) {
-		return Response.json(
-			{ message: 'You do not have access to this feature' },
-			{ status: 403 },
-		);
-	}
-
-	const userId = user.id;
-
-	const meetingId = searchParams.get('meeting-id');
-
-	if (!meetingId) {
-		return Response.json(
-			{ error: 'File query parameter is required' },
-			{ status: 400 },
-		);
-	}
-	let meeting;
 	try {
-		meeting = await db.meeting.update({
-			where: {
-				id: meetingId,
-				userId: userId,
-			},
+		const user = await currentUser();
+		if (!user) {
+			return Response.json({ message: 'Unauthorized' }, { status: 401 });
+		}
+
+		if (!user.isEarlyAccess) {
+			return Response.json(
+				{ message: 'You do not have access to this feature' },
+				{ status: 403 },
+			);
+		}
+
+		const userId = user.id;
+		if (!userId) {
+			return Response.json({ message: 'User ID not found' }, { status: 400 });
+		}
+		const meeting = await db.meeting.create({
 			data: {
+				id: uuidv4(),
+				userId: userId,
 				status: MeetingStatus.InProgress,
+				date: new Date(),
+				time: new Date().getHours() + ':' + new Date().getMinutes(),
+				duration: 60,
+				meetingLink: 'N/A',
+				title: 'On demand meeting: ' + new Date().toISOString(),
+				description: 'On demand meeting: ' + new Date().toISOString(),
+				provider: 'Externsion',
 			},
+		});
+
+		if (!meeting) {
+			return Response.json(
+				{ message: 'Meeting cannot be created' },
+				{ status: 400 },
+			);
+		}
+
+		const { url } = await generatePresignedUrl({
+			key: meeting.id,
+			bucketType: S3BucketType.RAW_RECORDINGS_BUCKET,
+			expiresIn: meeting.duration * 60 + 10 * 60,
+		});
+
+		return Response.json({
+			presignedUrl: url,
+			meeting: meeting,
 		});
 	} catch (error) {
 		console.error(error);
-		return Response.json({ message: 'Meeting not found' }, { status: 404 });
+		return Response.json({ message: 'Internal Server Error' }, { status: 500 });
 	}
-
-	if (!meeting) {
-		return Response.json({ message: 'Meeting not found' }, { status: 404 });
-	}
-
-	const { url } = await generatePresignedUrl({
-		key: meetingId,
-		bucketType: S3BucketType.RAW_RECORDINGS_BUCKET,
-		expiresIn: meeting.duration * 60 + 10 * 60,
-	});
-
-	return Response.json({
-		presignedUrl: url,
-		meeting: meeting,
-	});
 }
