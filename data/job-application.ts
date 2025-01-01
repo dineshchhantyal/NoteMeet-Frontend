c635c3c0a1c80437b00c55c6e4202f3f28c7b4a3
Dinesh Chhantyal <myagdichhantyal@gmail.com>
2025-01-01 00:18:44 -0600

    chore: remove unused create-job-application.ts file to clean up the codebase

---
+++ data/job-application.ts
@@ -0,0 +1,83 @@
+'use server';
+
+import { currentRole } from '@/lib/auth';
+import { db } from '@/lib/db';
+import { UserRole } from '@prisma/client';
+
+export const getAdminJobApplications = async () => {
+	try {
+		await checkAdmin();
+		const jobApplications = await db.jobApplication.findMany();
+
+		return jobApplications;
+	} catch (error) {
+		console.error('Error fetching job applications:', error);
+		return [];
+	}
+};
+
+export const getAdminJobApplicationById = async (id: number) => {
+	try {
+		await checkAdmin();
+		const jobApplication = await db.jobApplication.findUnique({
+			where: { id },
+		});
+
+		return jobApplication;
+	} catch (error) {
+		console.error('Error fetching job application:', error);
+		return null;
+	}
+};
+
+export const updateAdminJobApplicationStatus = async (
+	id: number,
+	status: number,
+) => {
+	try {
+		await checkAdmin();
+		const jobApplication = await db.jobApplication.update({
+			where: { id },
+			data: { status },
+		});
+
+		return jobApplication;
+	} catch (error) {
+		console.error('Error updating job application:', error);
+		return null;
+	}
+};
+
+export const deleteAdminJobApplication = async (id: number) => {
+	try {
+		await checkAdmin();
+		const jobApplication = await db.jobApplication.delete({ where: { id } });
+
+		return jobApplication;
+	} catch (error) {
+		console.error('Error deleting job application:', error);
+		return null;
+	}
+};
+
+export const deleteAdminManyJobApplications = async (ids: number[]) => {
+	try {
+		await checkAdmin();
+		const jobApplications = await db.jobApplication.deleteMany({
+			where: { id: { in: ids } },
+		});
+
+		return jobApplications;
+	} catch (error) {
+		console.error('Error deleting job applications:', error);
+		return null;
+	}
+};
+
+const checkAdmin = async () => {
+	const role = await currentRole();
+
+	if (!role || role !== UserRole.ADMIN) {
+		throw new Error('Unauthorized');
+	}
+};
