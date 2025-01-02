680b1d758b142c1830f5df22b440e1100e7bac04
Dinesh Chhantyal <myagdichhantyal@gmail.com>
2025-01-02 10:08:07 -0600

    style: update grid layout in MeetingInfo component for improved responsiveness on larger screens

--- app/(protected)/(playground)/dashboard/components/meeting-info.tsx
+++ app/(protected)/(playground)/dashboard/components/meeting-info.tsx
@@ -56,7 +56,7 @@ export function MeetingInfo({ meeting, onMeetingDelete }: MeetingInfoProps) {
 						))}
 					</div>
 				</div>
-				<div className="grid grid-cols-2 md:grid-cols-4 gap-2">
+				<div className="grid grid-cols-2 lg:grid-cols-4 gap-2">
 					<TooltipProvider>
 						<Tooltip>
 							<TooltipTrigger asChild>
